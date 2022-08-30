961f7d1f67e36fd9ce41f1be08ecce61c8fe75f9
alexandruale26 <alexandru_ale26@yahoo.com>
2022-08-30 10:25:27 +0300

    Add project files.

---
+++ OOP_ALGORITMICA_P/Dulap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace OOP_ALGORITMICA_P
+{
+    public class Dulap
+    {
+        private readonly int lungime;
+        private readonly int latime;
+        private readonly int inaltime;
+        public readonly int volum; // volumul poate fi citit de oriunde fiinda e calculat deja 
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+            this.volum = lungime * latime * inaltime; // volumul e creat automat in constructor
+        }
+    }
+}
---
+++ OOP_ALGORITMICA_P/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP_ALGORITMICA_P
+{
+    public class Program
+    {
+        static void Main()
+        {
+            
+            Dulap dulap = CreazaDulap();
+            Console.WriteLine($"Volumul dulapului este {dulap.volum}");
+        }
+
+
+        private static Dulap CreazaDulap()
+        {
+            Console.WriteLine("Introduce-ti dimensiunile unui dulap !");
+
+            Console.WriteLine("Setati lungimia dulapu-lui !");
+            int lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Setati latimea dulapu-lui !");
+            int latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Setati inaltimea dulapu-lui !");
+            int inaltime = int.Parse(Console.ReadLine());
+            
+            return new Dulap(lungime, latime, inaltime);
+        }
+    }
+}
